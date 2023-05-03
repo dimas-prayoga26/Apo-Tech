5a973edaf88b1bc4c4d538ecb5e2a2e91d36afb6
Fatwah Firmansyah <fatwah091@gmail.com>
2023-05-03 22:45:17 +0700

    Make API for Authentication

---
+++ app/Http/Controllers/api/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    protected function auth(Request $request){
+        $loginData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt($loginData)){
+            $token = Auth::user()->createToken('authToken')->plainTextToken;
+            $user = array_merge(Auth::user()->toArray(), ['token' => $token]);
+            return $this->okResponse('Login berhasil', $user);
+        }
+        
+        return $this->unauthenticatedResponse('Login Gagal!');
+    }
+}
