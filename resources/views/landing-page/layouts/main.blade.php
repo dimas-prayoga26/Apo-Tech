b42e79c9cf88632b66c9df0ca2577b51f51422a5
dimas-prayoga26 <dimasvorx117@gmail.com>
2023-05-20 08:06:44 +0700

    Progressing Profile

--- resources/views/landing-page/layouts/main.blade.php
+++ resources/views/landing-page/layouts/main.blade.php
@@ -8,45 +8,48 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-cYrNjLb6U5Sw6U11aMR7FWRL0jlvTV15zJd1gpXG1ZVGLjv0Jez7VszSXfA8DW7oOgjeBVqGxqQJ9XrBOfjU1g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{ asset('assets-ui/fontawesome/css/all.min.css') }}">
-
+  {{-- <link rel="stylesheet" href="{{ asset('assets-ui/.css') }}"> --}}
   @yield('css')
 
 </head>
 <body class="defult-landing-page">
   <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-color">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#" style="margin-right: 150px;margin-left: 50px;">Logo</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">    
-                <li class="nav-item mr-5">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}" style="margin-right: 100px;"><i class="fas fa-home me-2"></i>Beranda</a>
-                </li>
-                <li class="nav-item mt-0" style="margin-top: 10px;">
-                    <form class="d-flex">
-                    <input style="width: 400px;" class="form-control me-3 w-100"  type="search" placeholder="Cari" aria-label="Cari" >
-                    <button class="btn btn-outline-info btn-outline-dark" type="submit" ><i class="fas fa-solid fa-magnifying-glass"></i></button>
-                    </form>
-                </li>
-                </ul>
-                <ul class="navbar-nav ms-auto " style="margin-right: 50px;">
-                    <li class="nav-item ms-lg-5" style="margin-right: 50px;">
-                        <a class="nav-link" href="#"><button class="btn btn-outline-info btn-outline-dark"><i class="fas fa-basket-shopping"></i></button></a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="{{ route('auth.login') }}">
-                        <button class="btn btn-outline-info btn-outline-dark">
-                            <i class="fas fa-user me-2"></i>Login
-                        </button>
-                    </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-color">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#" style="margin-right: 150px; margin-left: 50px">Logo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item mr-5 mt-1">
+            <a class="nav-link active" aria-current="page" href="#" style="margin-right: 100px"><i class="fas fa-home me-2"></i>Beranda</a>
+          </li>
+          <li class="nav-item mt-2">
+            <form class="d-flex">
+              <input style="width: 300px" class="form-control me-2" type="search" placeholder="Cari" aria-label="Cari" />
+              <button class="btn btn-outline-info btn-outline-dark" type="submit"><i class="fas fa-solid fa-magnifying-glass"></i></button>
+            </form>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto" style="margin-right: 50px">
+          <li class="nav-item ms-lg-5" style="margin-right: -25px">
+            <a class="nav-link" href="#"><button class="btn btn-outline-info btn-outline-dark"><i class="fas fa-basket-shopping"></i></button></a>
+          </li>
+          <li class="nav-item ms-lg-5" style="margin-right: 50px">
+            <a class="nav-link" href="#"
+              ><button class="btn btn-outline-info btn-outline-dark"><i class="fa-sharp fa-regular fa-bell"></i></button
+            ></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <button class="btn btn-outline-info btn-outline-dark"><i class="fas fa-user me-2"></i>Login</button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
     <div class="main-content">
         @yield('content')
     </div>
--- resources/views/landing-page/profile/product-order-information/index.blade.php
+++ resources/views/landing-page/profile/product-order-information/index.blade.php
@@ -93,7 +93,62 @@
                                   <span class="input-group-text" id="basic-addon1" style="border-radius: 0px;"><i class="fa-solid fa-magnifying-glass" style="height: 25px;padding-top: 5px;"></i></span>
                                 </div>
                                 <input style="width: 400px;margin-right: 25px;" type="text" class="form-control custom-input" placeholder="search" aria-label="Username" aria-describedby="basic-addon1">
-                            </div>                              
+                            </div>   
+                            <div style="border-radius: 25px; margin-left: 25px; margin-right: 25px" class="border">
+                                <div class="d-flex justify-content-between border-bottom border-black align-items-center px-4">
+                                  <p class="mt-2">11, april 2023, 20,12</p>
+                                  <div>
+                                    <button class="btn btn-sm btn-warning" style="border-radius: 25px; color: white">Regular</button>
+                                  </div>
+                                </div>
+                                <div>
+                                  <table class="table table-borderless">
+                                    <tr>
+                                      <td>Nomor Transaksi</td>
+                                      <td>Status</td>
+                                      <td>Nama Penjual</td>
+                                      <td>Total Pembelanjaan</td>
+                                    </tr>
+                                    <tr>
+                                      <td>
+                                        <b>#104677127</b>
+                                      </td>
+                                      <td>Selesai</td>
+                                      <td>Apotek Kelapa Gading</td>
+                                      <td>Rp 21.186</td>
+                                    </tr>
+                                  </table>
+                                </div>
+                                <div class="d-flex border">
+                                  <div class="p-4 w-100">
+                                    <div class="d-flex w-100" style="border-bottom: 1px dashed rgb(229, 229, 229)">
+                                      <div class="col-xl-4 col-lg-4 col-md-4 d-flex">
+                                        <img src="{{ asset('assets-image/images-landing/milk.png') }}" alt="">
+                                        <div class="d-inline ml-4 mt-0 m-b-8">
+                                          <!---->
+                                          <!---->
+                                          <div class="font-weight-bold text-uppercase m-b-8">
+                                            <a href=""><img src="" alt=""></a> <b>Susu</b> </a>
+                                          </div>
+                                          <span class="text-orange f-size-12">Rp. 3.284</span><br /><span>1 Produk</span>
+                                        </div>
+                                      </div>
+                                      <div class="col-xl-4 col-lg-4 col-md-4 mt-3">
+                                        <div class="font-weight-light m-b-8">Total Harga Produk</div>
+                                        <p class="font-weight-bold m-b-8 text-orange f-size-14">Rp. 3.284</p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div style="border-radius: 0 0 25px 25px" class="p-12-0 d-flex justify-content-between px-4 py-2">
+                                  <div class="col-xl-6 col-lg-6 col-md-6 text-gr">
+                                    <p class="font-12 font-12-md mb-0">&nbsp;<a class="conge" href="" style="text-decoration: none; color: black"><i class="fa-solid fa-file-invoice"></i>&nbsp;Lihat Detail Pesanan</a></p>
+                                  </div>
+                                  <div class="col-xl-6 col-lg-6 col-md-6 text-gray-muted text-right">
+                                    <p class="font-12 font-12-md mb-0">&nbsp;<a class="conge" href="" style="text-decoration: none; color: black"><i class="fa-regular fa-comment-dots"></i>&nbsp;Chat Customer Service/Pharmacist</a></p>
+                                  </div>
+                                </div>
+                            </div>                           
                         </div>
                     </div>
                 </div>
